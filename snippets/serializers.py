c9c917134ea2e114ec9c664084ff9aeb249ad4fc
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2017-07-06 09:08:36 +0500

    charfield vs hyperlink

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -14,18 +14,30 @@ class SnippetSerializer(serializers.HyperlinkedModelSerializer):
                   'code', 'linenos', 'language', 'style', 'owner',)
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    snippets = serializers.HyperlinkedRelatedField(
-        many=True, view_name='snippet-detail', 
-        queryset=Snippet.objects.all())
-
-    class Meta:
-        model = User
-        fields = ('url', 'id', 'username', 'snippets',)
-
-# class UserSerializer(serializers.Serializer):
-#     username = serializers.CharField()
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
 #     snippets = serializers.HyperlinkedRelatedField(
-#         many=True, view_name='snippet-detail', read_only=True)
+#         many=True, view_name='snippet-detail', 
+#         queryset=Snippet.objects.all())
 
+#     class Meta:
+#         model = User
+#         fields = ('url', 'id', 'username', 'snippets',)
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail',
+        queryset=Snippet.objects.all())
+    # snippets = serializers.CharField()
+    # snippet_code = serializers.CharField()
+
+    def create(self, validated_data):
+        """
+        Create and return a new `User` instance, given the validated data.
+        """ 
+        print(validated_data)
+        user = User.objects.create_user(validated_data['username'])
+        snippet = Snippet.objects.create(code=validated_data['snippets'], owner=user)
+        snippet.save()
+        return user
     
